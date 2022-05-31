c04e59d1006e81c8f9cb3368c9da33da4a729cb0
BiggerRain <15911122312@163.COM>
2022-05-31 22:48:57 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,24 @@
+const { defaultTheme } = require('@vuepress/theme-default')
+
+module.exports = {
+  lang: 'zh-CN',
+  title: '你好， 欢迎来到 Rain9 基地 ！',
+  description: '这是我的个人站点',
+  theme: defaultTheme({
+    logo: 'https://vuejs.org/images/logo.png',
+    // 默认主题配置
+    navbar: [
+      {
+        text: '首页',
+        link: '/',
+      },
+      {
+        text: 'Rain9 的博客',
+        children: [
+          { text: 'Github', link: 'https://github.com/RainyNight9' },
+          { text: '掘金', link: 'https://juejin.cn/user/1943592288391496/posts' }
+        ],
+      }
+    ],
+  })
+}
---
+++ docs/.vuepress/dist/assets/index.html.605e1d34.js
@@ -0,0 +1,1 @@
+import{_ as s,o as a,c as r,a as e,b as t}from"./app.e3b03640.js";const o={},c=e("h1",{id:"hello-vuepress",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#hello-vuepress","aria-hidden":"true"},"#"),t(" Hello VuePress")],-1),n=[c];function l(_,d){return a(),r("div",null,n)}var h=s(o,[["render",l],["__file","index.html.vue"]]);export{h as default};
